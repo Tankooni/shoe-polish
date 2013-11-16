261a0af2539292af515b0e678b45b7cca31b6a77
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2013-11-16 05:11:49 -0500

    Heys, asteroids?

---
+++ Testing/World/Sector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Punk.Graphics;
+using Punk.Tweens.Misc;
+using Punk.Utils;
+
+namespace Punk
+{
+    class Sector : Entity
+    {
+        protected Image background;
+        protected uint SectorSize = 1000;
+
+        public Sector()
+        {
+            Graphic = background = Image.CreateRect(SectorSize, SectorSize, FP.Color(FP.Rand(uint.MaxValue)));
+            Height = Width = (int)SectorSize;
+           
+        }
+        public override void Added()
+        {
+            base.Added();
+            for (int x = 0; x < 40; x++ )
+                World.Add(new Asteroid(FP.Rand(SectorSize) + X, FP.Rand(SectorSize) + Y));
+        }
+    }
+}
